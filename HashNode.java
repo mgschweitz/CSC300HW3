8646e9da28d8d30f68fcbe8570523fcaf0fcdb6d
mgschweitz <michael.schweitzer@cuw.edu>
2022-12-15 00:23:46 -0600

    hash 12

---
+++ HashNode.java
@@ -0,0 +1,16 @@
+
+public class HashNode 
+{
+	public String Key;
+	public String Value;
+	public int Code;
+	public HashNode Next;
+	
+	public HashNode(String key, String value, int code) 
+	{
+		this.Key = key;
+		this.Value = value;
+		this.Code = code;
+		this.Next = null;
+	}
+}
--- MainTwo.java
+++ MainTwo.java
@@ -1,22 +1,63 @@
+import java.util.Random;
 
 public class MainTwo 
 {
-
-	public static void main(String[] args) 
+	public static int randomize(int min, int max)
 	{
-		StackMin x = new StackMin();
+		Random rd = new Random();
+		int num = rd.nextInt(max-min) + min;
+		return num;
+	}
+	
+	public static void main(String[] args)
+	{
+//		StackMin x = new StackMin();
+//		
+//		x.push(12);
+//		x.push(4);
+//		x.push(3);
+//		x.min();
+//		x.push(15);
+//		x.push(18);
+//		x.min();
+//		x.pop();
+//		x.pop();
+//		x.pop();
+//		x.min();
 		
-		x.push(12);
-		x.push(4);
-		x.push(3);
-		x.min();
-		x.push(15);
-		x.push(18);
-		x.min();
-		x.pop();
-		x.pop();
-		x.pop();
-		x.min();
+		
+//----------------------------------------------------------------------------------------
+//		Hash Table Assignment #12 starts here
+		String[] keyList = new String[10000];
+		for (int i = 0; i < keyList.length; i++)
+		{
+			int r = randomize(0, 1000000);
+			String s = Integer.toString(r);
+			keyList[i] = s;
+			System.out.print(keyList[i] + " ");
+		}
+		System.out.println("---------------------------------------------------------------------------------");
+
+		long startTime;
+		long endTime;
+		
+		Danny table = new Danny();
+
+		for(int i =0; i < keyList.length-1; i++)
+		{
+			table.Add(keyList[i], Integer.toString(i));
+		}
+		
+		startTime = System.currentTimeMillis();
+		for(int i = 0; i < 100; i++)
+		{
+			int num = randomize(0, 100);
+			System.out.println("-----------------");
+			System.out.println("(" + table.Get(keyList[num]).Value + " is key " + keyList[num] + ") ");
+		}
+		endTime = System.currentTimeMillis();
+		
+		System.out.println("Time search runs is " + (endTime - startTime) + " milliseconds");
 	}
 
 }
